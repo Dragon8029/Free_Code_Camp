fd8fc8f5f67be25c4f3e63840e23ee4eedacf76a
Cam Utz <Dragon8029@gmail.com>
2019-03-18 11:38:29 -0700

    add apply-functional-programming-to-convert-strings-to-url-slugs.json

--- Code workthrough/javascript.js
+++ Code workthrough/javascript.js
@@ -1,10 +1,16 @@
-function sliceArray(anim, beginSlice, endSlice) {
-    // Add your code below this line
-    var newArr = anim.slice(beginSlice, endSlice);
-    
-    // Add your code above this line
-  }
-  var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
-  sliceArray(inputAnim, 1, 3);
+// the global variable
+var globalTitle = "Winter Is Coming";
+
+// Add your code below this line
+function urlSlug(title) {
+  return title.split(" ").join(/\-/).toLowerCase();
+  
+}
+// Add your code above this line
+
+var winterComing = urlSlug(globalTitle); // Should be "winter-is-coming"
+
+console.log(globalTitle);
+
 
 
